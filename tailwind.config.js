9f84ec45575db6cab115e91b6e82de682cf2a5bd
Shivam Raje <shivamraje848@gmail.com>
2024-09-18 11:43:33 +0530

    changes done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 tailwind.config={
   theme:{
+    extend:{
     gridTemplateColumns:{
       'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+    },
+    fontFamily:{
+      Outfit:[ "Outfit", "sans-serif"],
+      Ovo : ["Ovo","serif"]
+    },
+    colors:{
+      lightHover:'#fcf4ff',
+      darkHover:'#2a004a',
+    }
     }
   }
 }
